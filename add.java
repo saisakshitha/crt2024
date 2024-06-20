e520ba5f410fa6d74ebcb1f4e33fd224fed951df
saisakshitha <sakshithadayyala@gmail.com>
2024-06-20 14:46:31 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,12 @@
+
+class Add{
+public static void main(String args[ ]){
+Scanner S=new Scanner(System.in);
+int a,b,c;
+a=S.next Int( );
+b=S.next Int( );
+c=a+b;import java.util.*;
+
+System.out.println(c);
+}
+}
