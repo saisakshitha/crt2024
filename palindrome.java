5ad501675c423f663f00ddfc0d4e670eb5f5e900
saisakshitha <sakshithadayyala@gmail.com>
2024-07-01 15:32:04 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Test{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.next();
+        String s2="";
+        for(int i=s1.length()-1;i>=0;i--){
+            s2=s2+s1.charAt(i);
+        }
+        if(s1.equals(s2))
+        System.out.println("Palindrome");
+        else
+        System.out.println("Not a Palindrome");
+    }
+}
