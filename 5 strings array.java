b80ca295b1b4360a9c4dbda9b0ecd3977d270a92
saisakshitha <sakshithadayyala@gmail.com>
2024-07-01 15:56:21 +0530

    Create 5 strings array.java

---
+++ 5 strings array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Test{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1[]=new String[5];
+        for(int i=0;i<5;i++){
+            s1[i]=s.next();
+        }
+        for(int i=0;i<5;i++){
+        System.out.println(s1[i]);
+        }
+    }
+}      
