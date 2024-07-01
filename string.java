f55a904b25d1e92512a019251bbdf556277b5a97
saisakshitha <sakshithadayyala@gmail.com>
2024-07-01 14:28:52 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Test{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1="java lang";
+        System.out.println(s1);
+        char s2[]=s1.toCharArray();
+        System.out.println(Arrays.toString(s2));
+        for(int i=0;i<s2.length;i++){
+       System.out.println(s2[i]);
+    }
+}
+}
