6e873b1e5f8f561e316644d780b969c06eac5177
saisakshitha <sakshithadayyala@gmail.com>
2024-07-03 13:53:09 +0530

    Create concatanation.java

---
+++ concatanation.java
@@ -0,0 +1,10 @@
+
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(20+40);
+        System.out.println("wel"+"come");
+        System.out.println(20+"python");
+    }
+}
