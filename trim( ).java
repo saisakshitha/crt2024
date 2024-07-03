36f7abbaef4888413352bf06d8a1efc1c517a1a2
saisakshitha <sakshithadayyala@gmail.com>
2024-07-03 13:34:07 +0530

    Create trim( ).java

---
+++ trim( ).java
@@ -0,0 +1,12 @@
+
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        String str="  Java  Lang  ";
+        System.out.println(str.length());
+        String str1=str.trim();
+        System.out.println(str1.length());
+        System.out.println(str1);
+    }
+}
