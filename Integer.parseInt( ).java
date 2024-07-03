87500339dc7611bb2a750f2aee2129c8983317c4
saisakshitha <sakshithadayyala@gmail.com>
2024-07-03 13:44:18 +0530

    Create Integer.parseInt( ).java

---
+++ Integer.parseInt( ).java
@@ -0,0 +1,11 @@
+
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        String str1="234";
+        String str2="563";
+        System.out.println(Integer.parseInt(str1)+Integer.parseInt(str2));
+    }
+        
+}
