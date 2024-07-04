9c982e9ff3d5e79cc1fa3f8beec41fe0f25d60ec
saisakshitha <sakshithadayyala@gmail.com>
2024-07-04 14:54:24 +0530

    Create stringuppercase.java

---
+++ stringuppercase.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str1=s.nextLine();
+        System.out.println(str1.toUpperCase());
+    }
+}
