bc1d273ff4b4773eab80f96e8cc4b83a958aaadc
saisakshitha <sakshithadayyala@gmail.com>
2024-07-05 16:23:39 +0530

    Create thiskeyword.java

---
+++ thiskeyword.java
@@ -0,0 +1,15 @@
+
+
+import java.util.*;
+class Test{
+        int a=10,b=20;
+        void show(int a,int b)
+        {
+        System.out.println(a+b);
+        }
+        public static void main(String args[])
+        {
+            Test t1=new Test();
+            t1.show(100,200);
+        }
+    }
